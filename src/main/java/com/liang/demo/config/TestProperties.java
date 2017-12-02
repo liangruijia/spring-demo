fdbeedd8caa68148a0795f8c290e67b0e07add43
liangruijia <981035753@qq.com>
2017-12-02 20:15:46 +0800

    初始化spring boot测试

---
+++ src/main/java/com/liang/demo/config/TestProperties.java
@@ -0,0 +1,32 @@
+package com.liang.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liangruijia
+ */
+@Component
+public class TestProperties {
+    @Value("${com.easy.springboot.h5perf.app.name}")
+    private String appName;
+    @Value("${com.easy.springboot.h5perf.app.version}")
+    private String version;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
---
+++ src/main/java/com/liang/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.liang.demo.controller;
+
+import com.liang.demo.config.TestProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author liangruijia
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    TestProperties testProperties;
+
+    @RequestMapping(value = "/hello")
+    public String helloWorld(){
+        return "hello world:" + testProperties.getAppName() + "," + testProperties.getVersion();
+    }
+}
